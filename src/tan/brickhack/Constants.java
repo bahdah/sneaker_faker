2bb2a1861b98889771ee3a0f2b3f779f84fb30fc
Bahdah Shin <bshin5304@gmail.com>
2017-02-12 18:13:39 -0500

    Update Constants.java

--- src/tan/brickhack/Constants.java
+++ src/tan/brickhack/Constants.java
@@ -27,16 +27,16 @@ public interface Constants {
     public static boolean AUDIO_FORMAT_BIG_ENDIAN = false; // true, false
 
     // Watson API Credentials
-    public static String SPEECH_TO_TEXT_USERNAME = "c1987993-a7d9-46fa-a4dc-f40f0516a486";
-    public static String SPEECH_TO_TEXT_PASSWORD = "TYdtF48Obwit";
-    public static String TEXT_TO_SPEECH_USERNAME = "13db127d-ecc6-449a-812f-1e6453e7bdd1";
-    public static String TEXT_TO_SPEECH_PASSWORD = "6Nr8fwzvnP45";
-    public static String NATURAL_LANG_CLASSIFIER_USERNAME = "4b273e00-c0dd-4d97-a34a-3021ad9124e8";
-    public static String NATURAL_LANG_CLASSIFIER_PASSWORD = "RgN6lSlmVgpk";
-    public static String NATURAL_LANG_CLASSIFIER = "f5bbc5x176-nlc-3275";
+    public static String SPEECH_TO_TEXT_USERNAME = "";
+    public static String SPEECH_TO_TEXT_PASSWORD = "";
+    public static String TEXT_TO_SPEECH_USERNAME = "";
+    public static String TEXT_TO_SPEECH_PASSWORD = "";
+    public static String NATURAL_LANG_CLASSIFIER_USERNAME = "";
+    public static String NATURAL_LANG_CLASSIFIER_PASSWORD = "";
+    public static String NATURAL_LANG_CLASSIFIER = "";
 
     // Forecast.io API Key
-    public static String FORECAST_IO_API_KEY = "6c0fd7059a6132475ae298f8de987516";
+    public static String FORECAST_IO_API_KEY = "";
     public static String CURRENT_LONGITUDE = "41.881832";
     public static String CURRENT_LATITUDE = "-87.623177";
 
